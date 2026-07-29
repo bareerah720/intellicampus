6e04d2134528ae946a5720e60e5bfedf3bb0eb6e
Bareera Arif <bareeraharif52@gmail.com>
2026-07-29 23:47:57 +0500

    Week 1 completed - Django setup with PostgreSQL and Custom User Model

---
+++ accounts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('student', 'Student'),
+        ('teacher', 'Teacher'),
+        ('hod', 'HOD'),
+        ('admin', 'Admin'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
+
+    def __str__(self):
+        return self.username
